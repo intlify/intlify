9136fcfbb830618c7fa149732da0d9c1b9c6450e
kazuya kawaguchi <kawakazu80@gmail.com>
2026-06-23 21:52:03 +0900

    fix: drop packages/utils

--- packages/utils/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This is just an example, so you can safely delete all default props below
- *
- * @returns A string that says "Hello, tsdown!"
- */
-export function fn() {
-  return 'Hello, tsdown!'
-}
