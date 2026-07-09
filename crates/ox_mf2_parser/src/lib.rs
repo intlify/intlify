02531b401fc5cebf4cced68657388cc8543b86f3
kazuya kawaguchi <kawakazu80@gmail.com>
2026-07-09 19:01:26 +0900

    release: v0.14.0-alpha.9

--- crates/ox_mf2_parser/src/lib.rs
+++ crates/ox_mf2_parser/src/lib.rs
@@ -41,7 +41,7 @@
 //!
 //! [message-format-wg]: https://github.com/unicode-org/message-format-wg
 
-#![doc(html_root_url = "https://docs.rs/ox_mf2_parser/0.14.0-alpha.8")]
+#![doc(html_root_url = "https://docs.rs/ox_mf2_parser/0.14.0-alpha.9")]
 
 mod api;
 mod diagnostic;
