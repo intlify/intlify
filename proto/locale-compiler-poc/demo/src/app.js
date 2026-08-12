8d95bbe218bab6d19822ed315a4f00eae954e2b3
kazuya kawaguchi <kawakazu80@gmail.com>
2026-08-13 02:16:32 +0900

    feat(poc): add catalogless locale demo

---
+++ proto/locale-compiler-poc/demo/src/app.js
@@ -0,0 +1,10 @@
+/**
+ * @license MIT
+ * @author kazuya kawaguchi (a.k.a. kazupon)
+ */
+
+const heading = document.querySelector('h1')
+heading.textContent = 'Welcome'
+
+const button = document.querySelector('#pay')
+button.textContent = 'Pay now'
